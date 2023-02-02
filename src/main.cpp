d22dd4d5bdd2f5978046c2f5c0dc3744d441b63d
Eric Apostal <ehapostal04@gmail.com>
2023-02-02 12:17:33 -0500

    sorta auton?

--- src/main.cpp
+++ src/main.cpp
@@ -121,16 +121,21 @@ void tick() {
 	pros::delay(20);
 }
 
-void move(int amount) {
+void move(int amount, bool reverse = false) {
     front_left_motor.set_brake_mode(E_MOTOR_BRAKE_HOLD);
     back_left_motor.set_brake_mode(E_MOTOR_BRAKE_HOLD);
     front_right_motor.set_brake_mode(E_MOTOR_BRAKE_HOLD);
     back_right_motor.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 
-    front_left_motor.move_velocity(300);
-    back_left_motor.move_velocity(300);
-    front_right_motor.move_velocity(300);
-    back_right_motor.move_velocity(300);
+    int dir_mod = -1; // reverse handling
+    if (!reverse) {
+        dir_mod = 1;
+    }
+
+    front_left_motor.move_velocity(300*dir_mod);
+    back_left_motor.move_velocity(300*dir_mod);
+    front_right_motor.move_velocity(300*dir_mod);
+    back_right_motor.move_velocity(300*dir_mod);
 
     pros::delay(amount * 500); // shitty ass code ik ik
 
@@ -169,8 +174,6 @@ void turn_deg(double deg) {
     back_left_motor.brake();
     front_right_motor.brake();
     back_right_motor.brake();
-
-    // Wait until both motors reach their target position
 }
 
 void auton() {
@@ -179,7 +182,8 @@ void auton() {
     conveyer_motor.move(200);
     pros::delay(280);
     conveyer_motor.brake();
-    // turn_deg(90);
+    move(1, true);
+    turn_deg(4);
 }
 
 int main() {
